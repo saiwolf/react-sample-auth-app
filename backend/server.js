f1c41eed5ccab95edf74d55fec14910a7115f472
Robert E. Cato <robertcato015@gmail.com>
2020-09-08 18:30:39 -0500

    Added dynamic config object for CORS origin client URL

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,14 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const errorHandler = require('./_middleware/error-handler');
 const morgan = require('morgan');
+const config = require('./config');
+const clientUrl = config.clientUrl || 'http://localhost:8080';
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:8082',
+    origin: clientUrl,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various smart TVs) choke on 204
     credentials: true
 }));
@@ -31,4 +33,5 @@ app.use(errorHandler);
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
+    console.log('Connections allowed from: ' + clientUrl);
 });
