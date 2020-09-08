238a87718ce91212f22ba2c7bc1f74f2e373beed
Robert Cato <rcato@ci.thibodaux.la.us>
2020-09-08 08:41:43 -0500

    Backend: Helpers and Middleware

---
+++ backend/_helpers/db.js
@@ -0,0 +1,18 @@
+const config = require('../config.json');
+const mongoose = require('mongoose');
+const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+
+// connect to Mongo
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+mongoose.Promise = global.Promise;
+
+// Exports
+module.exports = {
+    Account: require('accounts/account.model'),
+    RefreshToken: require('accounts/refresh-token.model'),
+    isValidId
+};
+
+function isValidId(id) {
+    return mongoose.Types.ObjectId.isValid(id);
+}
